5c1f45feac1e356324f5d539be206f80eeaa133c
jstaursky <jstaursky@gmail.com>
2022-02-21 17:17:50 -0500

    closer to emulation figured out.

--- tests/example_two/example_two.cpp
+++ tests/example_two/example_two.cpp
@@ -32,37 +32,6 @@ public:
     void doEmulation();
 };
 
-// A callback that terminates the emulation
-class TerminateCallBack : public BreakCallBack {
-public:
-    virtual bool addressCallback(const Address &addr);
-};
-
-bool TerminateCallBack::addressCallback(const Address &addr)
-
-{
-    emulate->setHalt(true);
-
-    return true;
-}
-
-
-// A callback that terminates the emulation
-class runCallBack : public BreakCallBack {
-public:
-    virtual bool addressCallback(const Address &addr);
-};
-
-bool runCallBack::addressCallback(const Address &addr)
-
-{
-    MemoryState *mem = static_cast<EmulateMemory *>(emulate)->getMemoryState();
-    AddrSpace *ram = mem->getTranslate()->getSpaceByName("ram");
-
-//    emulate->setExecuteAddress(Address(ram, ));
-
-    return true;
-}
 
 
 class Binary
@@ -90,6 +59,11 @@ public:
         sleighroot = docstorage.openDocument (*cpu)->getRoot();
         docstorage.registerTag (sleighroot);
         trans->initialize (docstorage); // Initialize the translator
+        context->setVariableDefault("addrsize",2);
+        context->setVariableDefault("opsize",1);
+        context->setVariableDefault("bit64",1);
+        context->setVariableDefault("longMode",1);
+
         loader->attachToSpace (trans->getDefaultCodeSpace());
         emu = new EmulateBfd(this);
     }
@@ -99,6 +73,56 @@ public:
         return trans->printAssembly (asm_, adr);
     }
 };
+
+// A callback that terminates the emulation
+class TerminateCallBack : public BreakCallBack {
+public:
+    virtual bool addressCallback(const Address &addr);
+};
+
+bool TerminateCallBack::addressCallback(const Address &addr)
+{
+    emulate->setHalt(true);
+    return true;
+}
+
+class keepalivecallback : public BreakCallBack {
+public:
+  virtual bool addressCallback(const Address &addr);
+};
+ 
+bool keepalivecallback::addressCallback(const Address &addr)
+{
+    // MemoryState *mem = static_cast<EmulateMemory *>(emulate)->getMemoryState();
+    // uint1 buffer[256];
+    // uint4 esp = mem->getValue("ESP");
+    // AddrSpace *ram = mem->getTranslate()->getSpaceByName("ram");
+ 
+    // uint4 param1 = mem->getValue(ram,esp+4,4);
+    // mem->getChunk(buffer,ram,param1,255);
+ 
+    // cout << (char *)&buffer << endl;
+ 
+    // uint4 returnaddr = mem->getValue(ram,esp,4);
+    // mem->setValue("ESP",esp+8);
+    // emulate->setExecuteAddress(Address(ram,returnaddr));
+ 
+    // MemoryState *mem = static_cast<EmulateMemory *>(emulate)->getMemoryState();
+    // uint1 buffer[256];
+    // uint4 esp = mem->getValue("RIP");
+    // AddrSpace *ram = mem->getTranslate()->getSpaceByName("ram");
+ 
+    // uint4 param1 = mem->getValue(ram,esp,4);
+    // mem->getChunk(buffer,ram,param1,1);
+ 
+    // cout << (char *)&buffer << endl;
+ 
+    // uint4 returnaddr = mem->getValue(ram,esp,4);
+    // mem->setValue("ESP",esp+8);
+    // emulate->setExecuteAddress(Address(ram,returnaddr));
+ 
+    return true;                        // This replaces the indicated instruction
+}
 
 auto EmulateBfd::doEmulation() -> void
 {
@@ -112,26 +136,24 @@ auto EmulateBfd::doEmulation() -> void
     memstate.setMemoryBank(&registerstate);
     memstate.setMemoryBank(&tmpstate);
 
-    BreakTableCallBack breaktable(binary->trans); // Set up the callback object
-
-    emulator = new EmulatePcodeCache(binary->trans, &memstate, &breaktable);
-    memstate.setValue("ESP",0xbffffffc);
-    emulator->setExecuteAddress(Address(binary->trans->getDefaultCodeSpace(),0x000004f0));  // Initial execution address
-
+    BreakTableCallBack breaktable(binary->trans);
+    EmulatePcodeCache emulator(binary->trans,&memstate,&breaktable);
+ 
+    keepalivecallback keepalivecallback;
     TerminateCallBack terminatecallback;
-    breaktable.registerAddressCallback(Address(binary->trans->getDefaultCodeSpace(),0x80482c8),&terminatecallback);
+    breaktable.registerAddressCallback(Address(binary->trans->getDefaultCodeSpace(),0x00001040),&keepalivecallback);
+    breaktable.registerAddressCallback(Address(binary->trans->getDefaultCodeSpace(),0x000011c4),&terminatecallback);
 
-    emulator->setHalt(false);
-
+    emulator.setExecuteAddress(Address(binary->trans->getDefaultCodeSpace(),0x00001040));  // Initial execution address
 
     AssemblyRaw assememit;
-    Address addr;
-    for(;;) {
-        addr = emulator->getExecuteAddress();
+    Address addr = emulator.getExecuteAddress();
+    emulator.setHalt(false);
+    do {
         auto len = binary->trans->printAssembly(assememit,addr);
+        emulator.executeInstruction();
         addr = addr + len;
-        emulator->executeInstruction();
-    }
+    } while(!emulator.getHalt());
 
 }
 
@@ -144,12 +166,12 @@ int main (int argc, char* argv[])
               << "to the start of the .text section of your binary.\n\n";
 
     Binary bin_code (argv[1], "default");
-    bin_code.initialize ("x86");
+    bin_code.initialize ("x86-64");
 
     EmulateBfd emu(&bin_code);
     emu.doEmulation();
 
-    // uintm vaddr = 0x000004f0;
+    // uintm vaddr = 0x00001040;
     // AssemblyEmit* assememit = new AssemblyRaw();
     // Address addr (bin_code.getSpace(), vaddr);
 
@@ -161,7 +183,7 @@ int main (int argc, char* argv[])
     // addr = addr + length;
     // length = bin_code.printAssembly (*assememit, addr);
 
-    // auto cpu = bin_code.foo();
+    // auto cpu = *bin_code.cpu;
 
     // for (auto i : cpu.context_docs) {
     //     std::cout << "file = " << i.filename << std::endl;
